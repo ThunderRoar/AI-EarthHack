7ad0757580da091a8c01b26a8fd1fda57e9012e2
sharpjd <59419827+sharpjd@users.noreply.github.com>
2024-01-06 16:45:52 -0500

    added components

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,10 +2,16 @@ import './App.css'
 
 import Info from './components/InfoPanel';
 import Eval from './components/EvalPane';
+import EvalPane from './components/EvalPane';
+import InfoPanel from './components/InfoPanel';
 
 function App() {
   return (
-    <div>App</div>
+    <div>
+      <h1>App</h1>
+      <InfoPanel></InfoPanel>
+      <EvalPane></EvalPane>
+    </div>
   )
 }
 
--- frontend/src/components/InfoPanel.jsx
+++ frontend/src/components/InfoPanel.jsx
@@ -1,3 +1,10 @@
+/*
+Include a header with a logo to the left side and a link to the github repo to the right side and link to the genai earth hackathon website
+Title of the Website (team name for now)
+A small description of the the functionality of the website
+Use good css
+*/
+
 import React from 'react'
 
 const InfoPanel = () => {
