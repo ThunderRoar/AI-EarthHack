7ba2deff415550b4febdb474724994a3b945f7d3
sharpjd <59419827+sharpjd@users.noreply.github.com>
2024-01-07 12:22:01 -0500

    description panel and auto fit box to width

---
+++ frontend/src/components/DescPanel.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Box from './Box.jsx'
+import './DescPanel.css'
+
+const DescPanel = () =>{
+    return(
+        <Box>
+            <p class="descPanelP">
+                Type in a problem and solution below to get a 3-point evaluation
+            </p>
+        </Box>
+    )
+}
+
+export default DescPanel;
