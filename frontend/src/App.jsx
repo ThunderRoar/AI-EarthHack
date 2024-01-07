1c2edc4f7e7e6cd3148f6ca6147bb4724d31302e
sharpjd <59419827+sharpjd@users.noreply.github.com>
2024-01-07 18:47:03 -0500

    entry list and sorting

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,6 +7,9 @@ import DescPanel from './components/DescPanel';
 import SortPanel from './components/SortPanel';
 
 function App() {
+
+  let entries = GetMockData();
+
   return (
     <main>
       <div className='main'>
@@ -17,7 +20,7 @@ function App() {
         <InfoPanel/>
         <DescPanel></DescPanel>
         <EvalPanel/>
-        <SortPanel isVisible = {true} parameter={GetMockData()}></SortPanel>
+        <SortPanel isVisible={true} data={entries}></SortPanel>
         <Footer/>
       </div>
     </main>
@@ -27,29 +30,34 @@ function App() {
 export default App;
 
 function GetMockData(){
-  return JSON.stringify(
+  return (
     [
       {
+        ID: 0,
         rankings: [1, 2, 3],
         problem: "foo",
         solution: "foobar"
       }, 
       {
+        ID: 1,
         rankings: [3, 2, 1],
         problem: "oof",
         solution: "oofrab"
       }, 
       {
+        ID: 2,
         rankings: [2, 3, 2],
         problem: "foofoo",
         solution: "foofoobar"
       }, 
       {
+        ID: 3,
         rankings: [3, 3, 3],
         problem: "foofoofoo",
         solution: "foofoofoobar"
       }, 
       {
+        ID: 4,
         rankings: [1, 1, 1],
         problem: "oofoofoof",
         solution: "bar"
--- frontend/src/components/SortPanel.jsx
+++ frontend/src/components/SortPanel.jsx
@@ -3,13 +3,65 @@ import Box from './Box.jsx'
 import './SortPanel.css'
 
 const SortPanel = ({isVisible, data}) =>{
+
+    console.log("data: " + data);
+    let sorted = GetSortedEntries(data={data});
+    console.log("sorted: " +sorted);
+
     return isVisible ? (
         <Box>
-            <Box>
-                
-            </Box>
+            {sorted}
         </Box>
     ) : null
+}
+
+const GetSortedEntries = ({data}) => {
+
+    console.log("GetSortedEntries: " + data);
+
+    let sorted = sortData(data);
+
+    let comps = [];
+    for(let i = 0; i < sorted.length; i++){
+
+        let entry = sorted[i];
+        let entryComp = Entry({entry});
+
+        comps.push(entryComp);
+    }
+
+    return comps;
+}
+
+const Entry = ({entry}) => {
+    return (
+        <Box>
+            <h3>{entry.ID}: {entry.problem}</h3>
+            <p>{entry.solution}</p>
+        </Box>
+    )
+}
+
+function sortData(data){
+    
+    /*
+    return [
+        {
+            ID: 69,
+            rankings: [1,2,3],
+            problem: "hello",
+            solution: "world"
+        }
+    ]
+    */
+    
+
+    return data.sort((first, second) => (averageRankingOf(first) < averageRankingOf(second) ? -1 : 1));
+
+    function averageRankingOf(entry){
+        let sum = entry.rankings[0]+entry.rankings[1]+entry.rankings[2];
+        return sum/3;
+    }
 }
 
 export default SortPanel;
