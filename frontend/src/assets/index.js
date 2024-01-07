2f02ccf0160c234f36f9ddd9c62291d10e007a12
Shreyas Rao <raoshreyas2004@gmail.com>
2024-01-06 21:29:36 -0500

    finished with eval panel display

--- frontend/src/assets/index.js
+++ frontend/src/assets/index.js
@@ -2,10 +2,12 @@ import copyIcon from './copy.svg'
 import loading from './loader.gif'
 import checkIcon from './tick.svg'
 import temp_logo from './temp_logo.png'
+import sendIcon from './send.png'
 
 export {
     copyIcon,
     loading,
     checkIcon,
-    temp_logo
+    temp_logo,
+    sendIcon
 }
--- frontend/src/components/EvalPanel.jsx
+++ frontend/src/components/EvalPanel.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { useState, useEffect } from 'react';
-import { loading, checkIcon, copyIcon } from '../assets';
+import { loading, checkIcon, copyIcon, sendIcon } from '../assets';
+import './EvalPanel.css';
 
 const EvalPanel = () => {
   return (
@@ -9,11 +10,37 @@ const EvalPanel = () => {
         <form 
         className="interactive" 
         onSubmit={() => {}}>
-          <div className='copyicon-css'>
-            <img className="copyicon" src={copyIcon} alt='copy-icon'/>
-          </div>
+          {/* <div className='input-box'> */}
+            <img className="copyIcon1-css" src={copyIcon} alt="copy" />
+
+            <input 
+            type="text"
+            value=""
+            placeholder='Enter the problem question' 
+            onChange={() => {}} 
+            required
+            className='problem-input'/>
+
+            <img className="copyIcon2-css" src={copyIcon} alt="copy" />
+
+            <input 
+            type="text"
+            value=""
+            placeholder='Enter your Solution' 
+            onChange={() => {}} 
+            required
+            className='soln-input'/>
+          {/* </div> */}
+          
+          <button type='submit' className='submit_button'>
+            <img className="send-icon" src={sendIcon} alt="" />
+          </button>
+
+
         </form>
       </div>
+
+      {/* Display resukts from the user input */}
     </section>
   );
 }
