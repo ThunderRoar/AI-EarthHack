861635f52e1fd7059c8ff5b150f373a3238dc528
sharpjd <59419827+sharpjd@users.noreply.github.com>
2024-01-07 19:02:52 -0500

    fleshed out Entry components

--- frontend/src/components/Entry.jsx
+++ frontend/src/components/Entry.jsx
@@ -6,6 +6,7 @@ const Entry = ({entry}) => {
         <Box>
             <h3>{entry.ID}: {entry.problem}</h3>
             <p>{entry.solution}</p>
+            <p>Novelty: {6-entry.rankings[0]}/5, Utility: {6-entry.rankings[1]}/5, Surprise: {6-entry.rankings[2]}/5</p>
         </Box>
     )
 }
