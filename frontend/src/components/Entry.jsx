a76329ff4242caad4b11ad22854557b0deb586c4
sharpjd <59419827+sharpjd@users.noreply.github.com>
2024-01-07 19:06:05 -0500

    ignore previous two commits, mistake on amend

--- frontend/src/components/Entry.jsx
+++ frontend/src/components/Entry.jsx
@@ -6,7 +6,7 @@ const Entry = ({entry}) => {
         <Box>
             <h3>{entry.ID}: {entry.problem}</h3>
             <p>{entry.solution}</p>
-            <p>Novelty: {entry.rankings[0]-5}/5, Utility: {entry.rankings[1]-5}/5, Surprise: {entry.rankings[2]-5}/5</p>
+            <p>Novelty: {6-entry.rankings[0]}/5, Utility: {6-entry.rankings[1]}/5, Surprise: {6-entry.rankings[2]}/5</p>
         </Box>
     )
 }
